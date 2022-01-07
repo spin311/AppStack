a162a907513bbc9b47ebc2812583f3a153d340a1
spin311 <svit.spindler@gmail.com>
2022-01-07 22:43:28 +0100

    koda

--- app.js
+++ app.js
@@ -1,60 +1,33 @@
-const express = require ('express');
+const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv/config');
-var app = express();
-var PORT = 3000;
+var port = 3000;
+
+const app = express();
 
 app.set('view engine', 'ejs');
 
-const { MongoClient } = require('mongodb');
-const uri = process.env.DB_CONNECTION;
-
 app.use(express.urlencoded({ extended: false }));
+
+// Connect to MongoDB
 mongoose
-.connect(
-  "mongodb://mongo:27017/docker-node-mongo", {useUnifiedTopology: true, useNewUrlParser: true}
-).then(() => console.log('connected with mongoose'));
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  );
+
 
 const Item = require('./models/Item');
 
-function calculateBMI() {
-
-
-  var visina = 100;
-  var debelina = 100;
-  
-  var bmi = 0;
-
-  if (visina === "" || isNaN(visina)) 
-       return null;
-  
-    else if (debelina === "" || isNaN(debelina)) 
-        return null;
-
-  else {
-   bmi = (debelina / ((visina * visina)  / 10000)).toFixed(2);
-   return bmi;
-  }
-
-
-}
-
 app.get('/', (req, res) => {
   Item.find()
-  .then(items => res.render('index', { items }))
-  .catch(err => res.status(404).json({ msg: 'No items found' }));
 });
 
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
-   bmi: calculateBMI()
-  }).then(() => console.log("Added BMI:", bmi));
-
-  newItem.save().then(item => res.redirect('/'));
-});
+// function datum() {
+//   var datum = new Date().toLocaleDateString("en-UK").replace(/\//g, '-');
+//   document.getElementById("dat").innerHTML = datum;
+// }
 
 
-app.listen(PORT, function(err){
-  if (err) console.log(err);
-  console.log("Server running on PORT:", PORT);
-}); 
+
+
+app.listen(port);
